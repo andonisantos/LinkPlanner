18a40a88352687bdecf5b7fc8ed4cfdce88d373b
AnaLuisaCarvalho <ana.luisa.mc@ua.pt>
2017-11-16 10:32:16 +0000

    sampler working with external clock; updated doc for m_qam_transmiter and m_qam_homodyne_receiver

--- lib/m_qam_homodyne_receiver.cpp
+++ lib/m_qam_homodyne_receiver.cpp
@@ -17,8 +17,8 @@ HomodyneReceiver::HomodyneReceiver(vector<Signal *> &inputSignal, vector<Signal 
 	B8.initializeBlock(vector<Signal*> { &HMD09 }, vector<Signal*> { &HMD11 }); //Filter
 	B9A.initializeBlock(vector<Signal*> { }, vector<Signal*> { &HMD12A }); //Clock
 	B9B.initializeBlock(vector<Signal*> { }, vector<Signal*> { &HMD12B }); //Clock
-	B10.initializeBlock(vector<Signal*> { &HMD10/*, &HMD12A*/ }, vector<Signal*> { &HMD13 }); //Sampler
-	B11.initializeBlock(vector<Signal*> { &HMD11/*, &HMD12B*/ }, vector<Signal*> { &HMD14 }); //Sampler
+	B10.initializeBlock(vector<Signal*> { &HMD10, &HMD12A }, vector<Signal*> { &HMD13 }); //Sampler
+	B11.initializeBlock(vector<Signal*> { &HMD11, &HMD12B }, vector<Signal*> { &HMD14 }); //Sampler
 	B12.initializeBlock(vector<Signal*> { &HMD13, &HMD14 }, vector<Signal*> { &HMD15 }); //Decoder
 
 	setModuleBlocks({ &B1, &B2, &B3, &B4, &B5, &B6, &B7, &B8, &B9A, &B9B, &B10, &B11, &B12 });
